551196659eae97a5ca55d4d588d5c0dfffd71310
Yakov Rakhamimov <jacobra19@gmail.com>
2020-02-26 00:46:24 +0200

    initial commit

---
+++ routes/comics.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var router = express.Router();
+
+const mongo = require('mongodb').MongoClient
+const url = process.env.MONGO_URI
+var db;
+
+mongo.connect(
+    url, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }, 
+    (err, client) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    db = client.db('yakovs-comics')
+    }
+)
+
+router.get('/', function(req, res, next) {
+    const collection = db.collection('comics')
+
+    collection
+    .find({})
+    .toArray()
+    .then((arr)=>{
+        // console.log('arr.length', arr.length)
+        res.send(arr)
+    })
+    .catch((err)=>{
+        // console.log('err', err)
+        res.send([])
+    })
+});
+
+
+router.get('/publishers/:value', (req, res)=> {
+    const collection = db.collection('comics')
+
+    collection
+    .find({'publish.title':req.params.value})
+    .toArray()
+    .then((data)=>{
+        if(data){
+            return res.send(data);
+        }
+    })
+    .catch((err)=>{
+        res.send([])
+    })
+});
+
+module.exports = router;
---
+++ routes/publishers.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+const mongo = require('mongodb').MongoClient
+const url = process.env.MONGO_URI
+var db;
+
+mongo.connect(
+    url, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }, 
+    (err, client) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    db = client.db('yakovs-comics')
+    }
+)
+
+
+
+/* GET publishers listing. */
+router.get('/', function(req, res, next) {
+    const collection = db.collection('comics')
+
+    collection
+    .distinct('publish.title')
+    .then((arr)=>{
+        res.send(arr)
+    })
+    .catch((err)=>{
+        res.send([])
+    })
+});
+
+
+module.exports = router;
