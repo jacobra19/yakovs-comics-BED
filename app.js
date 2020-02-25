23e8474c73befea8f5a58a63cc2256b52fcda36f
Yakov Rakhamimov <jacobra19@gmail.com>
2020-02-26 00:59:42 +0200

    Access-Control-Allow-Origin test

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ var publishersRouter = require('./routes/publishers');
 
 var app = express();
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-}
-app.use(allowCrossDomain);
+// var allowCrossDomain = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', "*");
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+// }
+// app.use(allowCrossDomain);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
