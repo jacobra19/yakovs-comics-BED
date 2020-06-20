63268bf484e9639c6f7296ea9e8edf87928b6c25
Yakov Rakhamimov <jacobra19@gmail.com>
2020-06-21 00:46:08 +0300

    mongoose: ComicBookModelSchema

--- routes/comicsMongoose.js
+++ routes/comicsMongoose.js
@@ -1,6 +1,41 @@
-// require('dotenv').config()
+require('dotenv').config()
 
-// const mongoose = require('mongoose');
-// const url = process.env.MONGO_CRED+process.env.MONGO_URI
+const mongoose = require('mongoose');
+const { ObjectID } = require('mongodb');
+const url = process.env.MONGO_CRED+process.env.MONGO_URI
 
-// mongoose.connect(url, {useNewUrlParser: true});
+mongoose.connect(url, {useNewUrlParser: true});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+var Schema = mongoose.Schema;
+
+var ComicBookModelSchema = new Schema({
+    _id: ObjectID,
+    publish: {
+        title: String,
+        date: String
+    },
+    media: {
+      coverSrc: String
+    },
+    series: {
+      title: String,
+      issue: String
+    },
+    description: String,
+    saga: {
+      title: String,
+      currentIssue: String,
+      totalIssues: String
+    },
+    variant: String,
+    creators: {
+      coverArtBy: [],
+      writtenBy: [],
+      pencilsBy: [],
+      inksBy: []
+    }
+});
